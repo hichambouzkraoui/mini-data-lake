6a8ffccb17255ac3ca3064b0a9b4cac93296e04d
Hicham Bouzkraoui <hichamm.bouzkraoui@gmail.com>
2025-10-27 23:10:31 -0600

    Initial commit

---
+++ test/mini-data-lake.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MiniDataLake from '../lib/mini-data-lake-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mini-data-lake-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MiniDataLake.MiniDataLakeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
