29376866a897e0c8a664924ecb9f692893818b31
Hicham Bouzkraoui <hichamm.bouzkraoui@gmail.com>
2025-10-28 11:28:00 -0600

    Added seeding

---
+++ test/constructs/data-deployment.test.ts
@@ -0,0 +1,44 @@
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { DataDeployment } from '../../lib/constructs/data-deployment';
+
+describe('DataDeployment', () => {
+  let app: cdk.App;
+  let stack: cdk.Stack;
+  let template: Template;
+  let rawBucket: s3.Bucket;
+
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new cdk.Stack(app, 'TestStack', {
+      env: { account: '123456789012', region: 'us-east-1' }
+    });
+    
+    rawBucket = new s3.Bucket(stack, 'RawBucket');
+    
+    new DataDeployment(stack, 'DataDeployment', {
+      rawBucket
+    });
+    
+    template = Template.fromStack(stack);
+  });
+
+  test('creates exactly 5 bucket deployments', () => {
+    template.resourceCountIs('Custom::CDKBucketDeployment', 5);
+  });
+
+  test('creates IAM roles for bucket deployments', () => {
+    template.hasResourceProperties('AWS::IAM::Role', {
+      AssumeRolePolicyDocument: {
+        Statement: [{
+          Effect: 'Allow',
+          Principal: {
+            Service: 'lambda.amazonaws.com'
+          },
+          Action: 'sts:AssumeRole'
+        }]
+      }
+    });
+  });
+});
